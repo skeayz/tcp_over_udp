81004a364f4d98125760751023bb9118d7afac62
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-23 09:41:18 +0100

    add security

--- src/main.py
+++ src/main.py
@@ -10,11 +10,24 @@ if __name__ == '__main__':
     if(len(sys.argv) != 2):
         print(f'{"Usage: python main.py <portNumber>"}')
         exit(1)
+        
     port = int(sys.argv[1])
+    if(port < 1000 or port > 9999):
+        print(f'{"Port number must be between 1024 and 65535"}')
+        exit(1)
+    
     print(f'{" Start of the TCP over UDP server on port  %d "%port:=^80}\n')
     print(f'{" Creation of the socket ":=^80}\n')
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(("", port))
-    threeway = threeway(s)
-    threeway.three_way_handshake()
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind(("", port))
+    except socket.error as e:
+        print(f'{"Error while creating the socket"}\n')
+        print(e)
+        exit(1)
+        
+    threeway = threeway(s, port)
+    
+    new_port : int = threeway.run()
     
