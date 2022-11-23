24d0c96e43a6b3a96ef40cd0fe133412d8ac150b
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-23 09:41:28 +0100

    better 3way

--- src/threeway.py
+++ src/threeway.py
@@ -1,21 +1,28 @@
 import socket
+import random
 
 class threeway:
 
-    def __init__(self, socket : socket.socket):
+    def __init__(self, socket : socket.socket, current_port : int):
         self.s = socket
+        self.current_port = current_port
 
-    def three_way_handshake(self):
+    def run(self) -> int:
         
-        message  = self.s.recvfrom(1024)
-        addr  = message[1]
-        message = message[0]
-        print("connexion établie avec", addr)
+        data, addr  = self.s.recvfrom(1024)
+        if(data.decode() != "SYN"):
+            raise Exception("SYN not received")
+        print(f'{" Received SYN ":=^80}\n')
         
-        self.s.sendto("SYN".encode())
-        print("SYN envoyé")
+        # Create new random port greater than current port but inferior to 9999
+        new_port = self.current_port + random.randint(1, 9999 - self.current_port)
+        #Create the Syn-ACk message including the new port
+        synack = f"SYN-ACK{str(new_port).zfill(4)}"
         
-        message = self.s.recvfrom(1024)
+        self.s.sendto(synack.encode(), addr)
+        print(f'{" Sent %s "%synack:=^80}\n')
+        
+        data, addr = self.s.recvfrom(1024)
         message = message[0]
         print(message)
         print("SYN-ACK reçu")
