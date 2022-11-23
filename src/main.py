80d5a9c462e1896e49c1e054a2a8b0ba5b4e1eb8
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-23 09:06:42 +0100

    add main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
+import sys
+import socket
+
+from threeway import *
+
+port : int
+s : socket.socket
 
 if __name__ == '__main__':
-    print(f'{" Start of the TCP over UDP server ":=^80}')
+    if(len(sys.argv) != 2):
+        print(f'{"Usage: python main.py <portNumber>"}')
+        exit(1)
+    port = int(sys.argv[1])
+    print(f'{"Port number : "}{port}')
+    print(f'{" Start of the TCP over UDP server":=^80}')
+    print(f'{" Creation of the socket ...":=^80}')
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(("", port))
+    threeway = threeway(s)
+    threeway.three_way_handshake()
+    
