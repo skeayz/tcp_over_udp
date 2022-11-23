f8d10fe5d09f93f606920f172ed303f21d9a52fc
skeayz <nicolas.guy@insa-lyon.fr>
2022-11-23 10:42:53 +0100

    demande fichier

--- src/sendfile.py
+++ src/sendfile.py
@@ -1,9 +1,13 @@
 import socket
+from utils import *
 class sendfile:
     
     def __init__(self, socket: socket.socket):
         self.s = socket
         
     def send_file(self, file):
-        with open(file, 'rb') as f:
-            self
+        data, addr  = self.s.recvfrom(1024)
+        print(f'[+] Reiceved : {custom_decode(data)} from {addr}')
+        
+        # with open(file, 'rb') as f:
+        #     self
