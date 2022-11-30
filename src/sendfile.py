dec3be2e7aa5118fac95ee5e98774c7ab3012578
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-30 11:52:46 +0100

    end file ok

--- src/sendfile.py
+++ src/sendfile.py
@@ -2,6 +2,7 @@ import socket
 from utils import *
 import threading
 import datetime
+import os
 class sendfile:
 
     lastAck = 0
@@ -9,6 +10,7 @@ class sendfile:
     window_size = 1
     seq = 1
     transfer = True
+    final_ack=0
 
     def __init__(self, socket: socket.socket, rtt: float):
         self.s = socket
@@ -22,6 +24,12 @@ class sendfile:
                 data, addr = socket.recvfrom(1024)
                 print(f'\t[+] Reiceved : {custom_decode(data)} from {addr}')
                 ack = int(custom_decode(data).replace("ACK", ""))
+                
+                if(ack == self.final_ack):
+                    self.s.sendto(custom_encode("FIN"), addr)
+                    print(f'\t[+] Sent : FIN to {addr}')
+                    break
+                
                 if(ack > self.lastAck):
                     self.lastAck = ack
                     self.window_size += 4
@@ -37,23 +45,24 @@ class sendfile:
                 
             except TimeoutError as err:
                 print(f'[-] Timeout')
-                self.seq -= 1
+                self.seq = self.seq - 1 if self.seq > 1 else 1
                 self.window_size = 1
 
     def run(self):
         print(f'\t\n{" Start of the file transfer ":=^80}\n')
         data, addr = self.s.recvfrom(1024)
         print(f'\t[+] Reiceved : {custom_decode(data)} from {addr}')
+        try:
+            f = open(custom_decode(data), 'rb')
+        except FileNotFoundError:
+            raise Exception("File not found")
+        f.seek(0, os.SEEK_END)
+        self.final_ack = int(f.tell()/1018) + 1
         th1 = threading.Thread(target=self.receive, args=(self.s,))
         th1.start()
         time_window: list(tuple) = []
         # start timer
         start = datetime.datetime.now().timestamp()
-        # check if file exists
-        try:
-            f = open(custom_decode(data), 'rb')
-        except FileNotFoundError:
-            raise Exception("File not found")
         # Send the file the client expects data messages that start with a sequence number, in string format, over 6 bytes, buffer is 1024 bytess
         while self.transfer:
             while self.window_size > 0:
@@ -65,11 +74,6 @@ class sendfile:
                         f'\t[+] Sent : {str(self.seq).zfill(6)} to {addr} with window size {self.window_size}')
                     time_window.append((datetime.datetime.now().timestamp() - start, self.window_size))
                     self.seq += 1
-                else:
-                    self.transfer = False
-                    self.s.sendto(custom_encode("FIN"), addr)
-                    print(f'\t[+] Sent : FIN to {addr}')
-                    break
                 self.window_size -= 1
         # print time_winfow into a file
         th1.join()
