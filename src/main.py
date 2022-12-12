3f68726b0707525fc6440e05bc49744bd52b9eba
skeayz <nicolas.guy@insa-lyon.fr>
2022-12-12 10:25:50 +0100

    fix: :bug: fix print error

--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ from sendfile import *
 
 if __name__ == '__main__':
     if(len(sys.argv) != 2):
-        print(f'{"Usage: python main.py <portNumber>"}')
+        print("Usage: python main.py <portNumber>")
         exit(1)
         
     port = int(sys.argv[1])
     if(port < 1000 or port > 9999):
-        print(f'{"Port number must be between 1024 and 65535"}')
+        print("Port number must be between 1024 and 65535")
         exit(1)
     
     print(f'{" Start of the TCP over UDP server on port  %d "%port:=^80}\n')
@@ -22,7 +22,7 @@ if __name__ == '__main__':
         three_way_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         three_way_socket.bind(("", port))
     except socket.error as e:
-        print(f'{"Error while creating the socket"}\n')
+        print("Error while creating the socket")
         print(e)
         exit(1)
     
