c00897afb25ff08fccf58c4a0172427eecaab2c2
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-13 22:28:56 +0100

    ad

--- src/sendfile.py
+++ src/sendfile.py
@@ -51,7 +51,7 @@ class sendfile:
                     self.lastAck = ack
                     self.duplicates = 0
 
-                if (ack == self.lastAck and self.duplicates < 2):
+                if (ack == self.lastAck and self.duplicates < 3):
                     self.duplicates += 1
                 if(self.duplicates >= 3):
                     print("DUPLICATES ACK FOR ACK " + str(self.lastAck))
