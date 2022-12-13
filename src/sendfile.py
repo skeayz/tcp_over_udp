f81bec0bc78bee9a39b2e67dfb1f2c443647cbda
skeayz <nicolas.guy@insa-lyon.fr>
2022-12-13 18:19:41 +0100

    test

--- src/sendfile.py
+++ src/sendfile.py
@@ -58,9 +58,9 @@ class sendfile:
                     print("DUPLICATES")
                     with self.lock:
                         self.seq = self.lastAck
-                        self.window_size = self.window_size // 2 if self.window_size > 1 else 1
-                        self.window_print = self.window_size
-                    self.lastAck = self.lastAck-1
+                        # self.window_size = self.window_size // 2 if self.window_size > 1 else 1
+                        # self.window_print = self.window_size
+                    # self.lastAck = self.lastAck-1
                     self.duplicates = 0
                 
                 
