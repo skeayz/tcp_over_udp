5f045b3613a4adb39ae4a7732193027dcfe73311
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-14 10:37:29 +0100

    szad

--- src/sendfile.py
+++ src/sendfile.py
@@ -63,9 +63,8 @@ class sendfile:
                         self.window_size = 1
                         self.window_print = self.window_size
                         self.last_duplicates = self.lastAck
+                        self.ss_tresh = (self.seq - self.lastAck) // 2 if (self.seq - self.lastAck) // 2 > 30 else 20
                     self.duplicates = 0
-
-                
                 
             except socket.error as err:
                 if(self.window_size >= 1):
