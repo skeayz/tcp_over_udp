a3c005e945ce082a904bcfe8819c193c6ebd6f58
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-14 10:29:11 +0100

    add sleep

--- src/sendfile.py
+++ src/sendfile.py
@@ -102,6 +102,7 @@ class sendfile:
         # Send the file the client expects data messages that start with a sequence number, in string format, over 6 bytes, buffer is 1024 bytess
         while self.transfer:
             while self.window_size > 0:
+                sleep(self.rtt)
                 with self.lock:
                     f.seek((self.seq-1)*self.buffersize)
                     data = f.read(self.buffersize)
