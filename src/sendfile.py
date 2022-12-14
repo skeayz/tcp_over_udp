e8ffdb3c6bd9396a1f7c9824c23de5504add3918
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-14 09:38:18 +0100

    b

--- src/sendfile.py
+++ src/sendfile.py
@@ -59,8 +59,8 @@ class sendfile:
                     with self.lock:
                         self.seq = self.lastAck + 1
                         self.window_size = 1
+                        self.ss_tresh = (self.seq - self.lastAck) // 2 if (self.seq - self.lastAck) // 2 > 30 else 20
                         self.window_print = self.window_size
-                        self.transfer = False
                     self.duplicates = 0
 
                 
