ed51c118339d12752346303dbceb7ff84f2b120b
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-14 17:36:33 +0100

    remove useless print

--- src/sendfile.py
+++ src/sendfile.py
@@ -111,7 +111,6 @@ class sendfile:
                         f.seek((self.lastAck)*self.buffersize)
                         sendseq = str(self.lastAck+1).zfill(6)
                     else:
-                        print(self.seq)
                         f.seek((self.seq-1)*self.buffersize)
                         sendseq = str(self.seq).zfill(6)
                         self.seq += 1
