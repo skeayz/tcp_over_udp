196d3f6f8a85d7dfef24e23865d50bfdc3ba8509
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-14 17:43:38 +0100

    correction in congestion avoidance

--- src/sendfile.py
+++ src/sendfile.py
@@ -46,8 +46,7 @@ class sendfile:
                         window_incr = (ack - self.lastAck) * 2 if (ack - self.lastAck) * 2 > 0 else 2
                     else:
                         with self.lock:
-                            window_incr = (self.window_size + 1/self.window_size)
-                            print("window_incr", window_incr)
+                            window_incr = 1/self.window_size
                     with self.lock:
                         self.window_size = (self.window_size + window_incr)
                         self.seq = ack + 1 if ack + 1 >= self.seq else self.seq
