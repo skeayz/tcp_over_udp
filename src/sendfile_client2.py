845fd6ceb6abb5b3eaf5a6335c4c4ad21e77aaed
skeayz <nicolas.guy@insa-lyon.fr>
2022-12-15 09:29:02 +0100

    test

--- src/sendfile_client2.py
+++ src/sendfile_client2.py
@@ -1,14 +1,17 @@
 import socket
 import math
+from time import sleep
 from utils import *
 import threading
-import datetime
+from datetime import datetime
 import os
+
+MAX_WINDOW_SIZE = 50
 class sendfile:
 
-    lastAck = 0
+    lastAck = -1
     duplicates = -1
-    window_size = 30
+    window_size = 1
     window_print = 1
     seq = 1
     transfer = True
@@ -16,73 +19,80 @@ class sendfile:
     rtt = 0.0
     lock = threading.Lock()
     buffersize = 1494
-    threshold = 30
+    ss_tresh = 10000000
+    last_duplicates = -2
 
-    def __init__(self, socket: socket.socket, rtt: float):
+    def __init__(self, socket, rtt):
         self.s = socket
         self.rtt = round(rtt * 1.3, 4)
-        self.s.settimeout(round(rtt * 5, 4))
+        
 
     def receive(self):
         ack = -1
-        time_window: list(tuple) = []
-        start = datetime.datetime.now().timestamp()
+        time_window = []
+        start = datetime.now()
         while ack != self.final_ack:
-            # flush the buffer
-            time_window.append((datetime.datetime.now().timestamp() - start, self.window_print))
+            time_window.append((datetime.now() - start, self.window_print))
             try:
-                data, addr = self.s.recvfrom(1024)
-                print(f'\t[+] Reiceved : {custom_decode(data)} from {addr}')
-                ack = int(custom_decode(data).replace("ACK", ""))
+                data, addr = self.s.recvfrom(1500)
+                print("[+] Reiceved : "+ str(custom_decode(data)) +" from " + str(addr))
+                ack = int(custom_decode(data).replace("ACK", ""))            
+                if(ack > self.lastAck or ack == self.last_duplicates):
+                    self.duplicates = 0
                 
-                if(ack > self.lastAck and self.threshold > self.window_size):
+                if(ack >= self.lastAck):
+                    if (self.ss_tresh > self.window_size):
+                        window_incr = (ack - self.lastAck) * 2 if (ack - self.lastAck) * 2 > 0 else 2
+                    else:
+                        with self.lock:
+                            window_incr = 1/self.window_size
                     with self.lock:
-                        self.window_size += (ack - self.lastAck) * 2
-                        self.seq = ack + 1
+                        self.window_size = (self.window_size + window_incr)
+                        self.seq = ack + 1 if ack + 1 >= self.seq else self.seq
                         self.window_print = self.window_size
                     self.lastAck = ack
-                    self.duplicates = 0
-                else:
-                    with self.lock:
-                        self.window_size += 1
-                        self.seq = ack + 1
-                        self.window_print = self.window_size
-                    self.lastAck = ack
-                    self.duplicates = 0
-                    
 
-                if (ack == self.lastAck and self.duplicates < 2):
-                    self.duplicates += 1
-                elif (ack == self.lastAck and self.duplicates >= 2):
-                    print("DUPLICATES")
+                if (ack == self.lastAck and ack != self.last_duplicates):
                     with self.lock:
-                        self.seq = self.lastAck
-                        self.window_size = self.window_size // 2 if self.window_size > 1 else 1
+                        self.duplicates += 1
+                if(self.duplicates >= 3):
+                    print("DUPLICATES ACK FOR ACK " + str(self.lastAck))
+                    with self.lock:
+                        self.seq = self.lastAck + 1
+                        self.window_size = 1
                         self.window_print = self.window_size
-                    self.lastAck = self.lastAck-1
-                    self.duplicates = 0
-                
-                
-            except socket.error as err:
-                print(f'[-] Timeout')
-                with self.lock:
-                    self.seq = self.seq - 1 if self.seq > 1 else 1
-                    self.window_size = self.window_size // 2 if self.window_size > 1 else 1
-                    self.window_print = self.window_size
+                        self.last_duplicates = self.lastAck
+                        self.duplicates = 0   
+            except socket.timeout:
+                if(self.window_size >= 1):
+                    print('[-] Timeout')
+                    with self.lock:
+                        self.ss_tresh = (self.seq - self.lastAck) // 2 
+                        self.seq = self.lastAck + 1 if self.lastAck > 0 else 1
+                        self.window_size = 1
+                        self.window_print = self.window_size
+                        self.duplicates = 0
                     
         ## When we receive the final ack we send end to the client
+        with self.lock:
+            self.transfer = False
+            self.window_size = 0
         self.s.sendto(custom_encode("FIN"), addr)
-        print(f'\t[+] Sent : FIN to {addr}')
-        self.window_size = 0
-        self.transfer = False
+        print('[+] Sent : FIN to' + str(addr))
+        print(datetime.now() - start, self.window_print)
         with open('time_window.txt', 'w') as f:
-            for time, window_size in time_window:
-                f.write(f'{time} {window_size}\n') 
+           for time, window_size in time_window:
+               f.write(str(time) + ' ' + str(window_size) + '\n') 
 
     def run(self):
-        print(f'\t\n{" Start of the file transfer ":=^80}\n')
-        data, addr = self.s.recvfrom(1024)
-        print(f'\t[+] Reiceved : {custom_decode(data)} from {addr}')
+        print(" Start of the file transfer ")
+        data = None
+        while not data:
+            try:
+                data, addr = self.s.recvfrom(1024)
+            except socket.timeout:
+                continue
+        print("[+] Reiceved : "+ str(custom_decode(data)) +" from " + str(addr))
         try:
             f = open(custom_decode(data), 'rb')
         except FileNotFoundError:
@@ -90,24 +100,25 @@ class sendfile:
         f.seek(0, os.SEEK_END)
         self.final_ack = math.ceil(f.tell()/self.buffersize)
         th1 = threading.Thread(target=self.receive)
+        th1.setDaemon(True)
         th1.start()
         # start timer
         # Send the file the client expects data messages that start with a sequence number, in string format, over 6 bytes, buffer is 1024 bytess
         while self.transfer:
             while self.window_size > 0:
+                sleep(self.rtt/2)
                 with self.lock:
-                    f.seek((self.seq-1)*self.buffersize)
-                    data = f.read(self.buffersize)
-                if(data):
-                    self.s.sendto(str(self.seq).zfill(6).encode() + data, addr)
-                    self.s.settimeout(round(self.s.gettimeout(), 4))
-                    print(
-                        f'\t[+] Sent : {str(self.seq).zfill(6)} to {addr} with window size {self.window_size}')
-                    with self.lock:
+                    if(self.last_duplicates == self.lastAck):
+                        f.seek((self.lastAck)*self.buffersize)
+                        sendseq = str(self.lastAck+1).zfill(6)
+                    else:
+                        f.seek((self.seq-1)*self.buffersize)
+                        sendseq = str(self.seq).zfill(6)
                         self.seq += 1
-                    with self.lock:
-                        self.window_size -= 1
-        # print time_window into a file
+                    self.s.settimeout((self.seq-self.lastAck)*self.rtt)
+                data = f.read(self.buffersize)
+                if(data):
+                    self.s.sendto(sendseq.encode() + data, addr)
+                    print('\t[+] Sent : '+sendseq+' to '+ str(addr) +' with window size '+str(self.window_size))
         th1.join()
-        print("coucou g join")
         exit(0)   
